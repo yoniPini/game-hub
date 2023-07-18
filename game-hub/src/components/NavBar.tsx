e84e0cf3248777670a3ccc2b3e90f247095e3f54
יונתן פנחס <yonatan.pinchas@live.biu.ac.il>
2023-07-18 10:31:00 +0300

    Build a NavBar

---
+++ game-hub/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { Text, HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/GameHub Resources/Logo/logo.webp";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>sdf</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
