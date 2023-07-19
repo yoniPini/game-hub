0eb67b1b407bd565af87eca5080261d4f0d0c7f6
יונתן פנחס <yonatan.pinchas@live.biu.ac.il>
2023-07-19 12:14:55 +0300

    build the color mode switch

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { Text, HStack, Image } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/GameHub Resources/Logo/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
-      <Text>sdf</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
